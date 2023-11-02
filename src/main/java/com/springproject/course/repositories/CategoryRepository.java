bc8b01c94c16fccad89f578ad2062145534dca97
Willian Comiotto <willianbcomiotto@gmail.com>
2023-11-02 20:09:31 -0300

    Category

---
+++ src/main/java/com/springproject/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.springproject.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springproject.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
